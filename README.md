bbe98bf60395075680932a8e323d66b8a816fd84
Tiago Dionesto Willrich da Silva <tiagodws@gmail.com>
2018-02-20 20:44:25 -0300

    heroku integration

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # react-book-list
 Projeto de lista de livors utilizando Redux, feito durante o curso https://www.udemy.com/react-redux/learn/v4/overview
+
+## Demonstração
+
+https://tiagodws-react-book-list.herokuapp.com/
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 
+import "../style/style.css";
+
 import App from "./components/app";
 import reducers from "./reducers";
 
